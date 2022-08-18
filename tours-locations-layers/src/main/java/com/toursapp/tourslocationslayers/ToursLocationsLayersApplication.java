243446e5940b500ac8261ea3e844d1c569a22f5e
YVic95 <vika.yevlentieva@gmail.com>
2022-08-18 13:38:26 +0300

    Tours, Locations, Layers service added

---
+++ tours-locations-layers/src/main/java/com/toursapp/tourslocationslayers/ToursLocationsLayersApplication.java
@@ -0,0 +1,13 @@
+package com.toursapp.tourslocationslayers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToursLocationsLayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToursLocationsLayersApplication.class, args);
+	}
+
+}
