190519e467ca05d619302bc0eb9a35234dab5c69
MKoth <lecorbeaulibre@gmail.com>
2022-08-19 13:35:42 +0300

    Added tag repository

---
+++ tours-locations-layers/src/main/java/com/toursapp/tourslocationslayers/repositories/TagRepository.java
@@ -0,0 +1,7 @@
+package com.toursapp.tourslocationslayers.repositories;
+
+import com.toursapp.tourslocationslayers.entities.Tag;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TagRepository extends CrudRepository<Tag, Integer> {
+}
