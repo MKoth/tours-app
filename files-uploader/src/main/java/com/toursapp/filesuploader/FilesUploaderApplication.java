e4e5d382d8f4db5a515e86a8e08e0498da0227b1
MKoth <lecorbeaulibre@gmail.com>
2022-08-31 15:02:07 +0300

    Added file uploader project

---
+++ files-uploader/src/main/java/com/toursapp/filesuploader/FilesUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.toursapp.filesuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesUploaderApplication.class, args);
+	}
+
+}
---
+++ files-uploader/src/test/java/com/toursapp/filesuploader/FilesUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toursapp.filesuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
